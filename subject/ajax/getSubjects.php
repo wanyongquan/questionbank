5d696340cf53afcf18f4af27faf088fd7be1cdf0
redredxcar <yongquanwan@hotmail.com>
2016-12-22 20:33:36 +0800

    subject should belong to a course

--- subject/ajax/getSubjects.php
+++ subject/ajax/getSubjects.php
@@ -3,14 +3,15 @@
 
     // write the table header;
     $data = '<table class="table table-striped table-hover">
-                <thead><tr><th>subject</th><th>Actions</th></tr></thead>
+                <thead><tr><th>知识点</th><th>课程</th><th>操作</th></tr></thead>
                 <tbody>';
-    $query = 'select * from tk_subjects order by subjectname;';
+    $query = 'select subject_id,coursename, subjectName from tk_subjects, tk_courses where tk_subjects.course_id = tk_courses.course_id order by subjectname;';
     $result = $DB->query($query) or die(exit(mysqli_error($DB)));
 
     if ($result->num_rows>0){
         foreach ($result as $row){
             $data .= '<tr><td>'.$row['subjectName'].'</td>';
+            $data .= '<td>'.$row['coursename'].'</td>';
             $data .= '<td><a title="Edit" onclick="getSubjectDetails('.$row['subject_id'].')"
                         data-toggle="modal" data-target="#edit_subject_modal" data-backdrop="false" >
                         <i class="glyphicon glyphicon-edit"></i></a>
